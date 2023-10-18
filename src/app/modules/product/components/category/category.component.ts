42d5197842386c058558c3bc146526bf19d56a72
Bryan Gonzalez <bryan.glz@ciencias.unam.mx>
2023-10-17 20:55:48 -0600

    Se agrego y modifico para cumplir las metas de la practica 8

--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -24,6 +24,7 @@ export class CategoryComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.formularioCategorias = this.formBuilder.group({
+      id: [null],
       category: ['', Validators.required],
       code: ['', Validators.required],
     });
@@ -34,22 +35,46 @@ export class CategoryComponent implements OnInit {
   }
 
   getCategories() {
-    this.categories.push(new category(this.lastid++, 'CP', 'Cuidado Personal', 'Activo'));
-    this.categories.push(new category(this.lastid++, 'JU', 'Jugueteria', 'Activo'));
-    this.categories.push(new category(this.lastid++, 'CJ', 'Centro de Jardineria', 'Error'));
+    this.categories.push(new category(this.lastid++, 'CP', 'Cuidado Personal', 1));
+    this.categories.push(new category(this.lastid++, 'JU', 'Jugueteria', 1));
+    this.categories.push(new category(this.lastid++, 'CJ', 'Centro de Jardineria', 0));
   }
 
   onSubmit() {
     if (this.formularioCategorias.valid) {
       const formData = this.formularioCategorias.value;
-      const nuevaCategoria = new category(this.lastid++, formData.code, formData.category, 'Procesando');
-
-      this.categories.push(nuevaCategoria);
+      console.log(formData);
+      if (formData.id != null) {
+        const temp = this.categories[formData.id-1];
+        temp.category = formData.category;
+        temp.code = formData.code;
+        this.categories[formData.id-1] = temp;
+      } else {
+        const nuevaCategoria = new category(this.lastid++, formData.code, formData.category, 0);  
+        this.categories.push(nuevaCategoria);
+      }
 
       $('#modalCategory').modal('hide');
-      this.formularioCategorias.reset();
+      this.resetModal();
     } else {
     }
+  }
+  
+  activate(id:number, status:number) {
+    const temp = this.categories[id-1];
+    temp.status = status;
+    this.categories[id-1] = temp;
+  }
+
+  editModal(editid:number, editcode:string, editcategory:string) {
+    this.formularioCategorias.setValue({id: editid, category: editcategory, code:editcode});
+    $('#modalCategory .modal-title').text("Editar Categoria");
+    $('#modalCategory').modal('show');
+  }
+
+  resetModal() {
+    $('#modalCategory .modal-title').text("Nueva Categoria");
+    this.formularioCategorias.reset();
   }
 
 }
